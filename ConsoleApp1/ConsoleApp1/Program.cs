e974dcb24ba5afc8c68c1970e7d35427a9cf789a
AbidovaGuler <gullarabidova@gmail.com>
2025-03-11 22:08:45 +0400

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Eded daxil edin : ");
+            int num = int.Parse(Console.ReadLine());
+            int hasil = 1;
+            while (num>0)
+            {
+                hasil *= num % 10; 
+                num /= 10;         
+            }
+            Console.WriteLine($"Reqemlerin hasili {hasil}");
+
+        }
+    }
+}

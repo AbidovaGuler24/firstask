ed90a3f25cf8c95b2d769dcd7171f26b781a2fe6
AbidovaGuler24 <gullaraa-ab109@code.edu.az>
2025-03-12 11:18:07 +0400

    Update Program.cs

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp4
             int[] arr = { 5, 7, 8, 9, 12, 23, 34 };
             Console.Write("Eded daxil edin: ");
             int n = int.Parse(Console.ReadLine());
-            bool isBiggerThanAllElement = true;
+            bool maxeded= true;
             for (int i = 0; i < arr.Length; i++)
             {
                 if (n<=arr[i])
